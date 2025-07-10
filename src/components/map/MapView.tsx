8eaa32dd1d4187ef64525e1375405e4ea14b39d6
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2025-07-10 17:07:30 +0300

    Adjust SVG marker icon vertical position by 2 units for better alignment

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -322,7 +322,7 @@ export default function MapView() {
       const svgMarkerString = `
         <svg xmlns='http://www.w3.org/2000/svg' width='32' height='42' viewBox='0 0 32 42' fill='none' color='white'>
           <path d='M16 42C16 42 28 26.5 28 18C28 8.05887 21.9411 2 16 2C10.0589 2 4 8.05887 4 18C4 26.5 16 42 16 42Z' fill='${iconColor}' stroke='white' stroke-width='2'/>
-          <g transform='translate(7,14) scale(0.8)'>
+          <g transform='translate(7,12) scale(0.8)'>
             ${iconPath}
           </g>
         </svg>
